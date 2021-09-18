6e02b0b2f548443f2addfdf9fe8bd683416c7e76
suhailasunu <suhailasunu03@gmail.com>
2021-09-18 17:34:28 +0400

    final

---
+++ node-sample-master/routes/user.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  let products=[
+    {
+      name:"iphone 11",
+      category:"mobile",
+      description:"this is a phone",
+      image:"https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone11-red-select-2019?wid=940&hei=1112&fmt=png-alpha&.v=1566956144763"
+    },
+    {
+      name:"iphone 11",
+      category:"mobile",
+      description:"this is a phone",
+      image:"https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone11-red-select-2019?wid=940&hei=1112&fmt=png-alpha&.v=1566956144763"
+    },
+    {
+      name:"iphone 11",
+      category:"mobile",
+      description:"this is a phone",
+      image:"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-12-family-select-2021?wid=940&hei=1112&fmt=jpeg&qlt=80&.v=1617135051000"
+    },
+    {
+      name:"iphone 11",
+      category:"mobile",
+      description:"this is a phone",
+      image:"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-12-family-select-2021?wid=940&hei=1112&fmt=jpeg&qlt=80&.v=1617135051000"
+    },
+  ]
+  
+  res.render('index', { products, admin:false });
+});
+
+module.exports = router;
